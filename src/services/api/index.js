1ca01f93bd51b065cc4d846057f3892962ea9c2e
chukwuemekachm <chukwuemekachm@gmail.com>
2020-03-28 18:25:43 +0100

    chore(deploy): deploy app

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const { API_URL = 'http://localhost:3000/api/v1' } = process.env;
+const API_URL = process.env.API_URL || 'http://localhost:3000/api/v1';
 
 const api = axios.create({
   baseURL: API_URL,
